2ccef09cac978c8ace6d944f4d7769dc9331d7ae
Shtekata <gesheval@gmail.com>
2025-09-19 20:18:15 +0300

    Adding some javascript

--- js/script.js
+++ js/script.js
@@ -38,8 +38,12 @@
 
 const tabs = document.querySelectorAll('.tab')
 const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
 
 tabs.forEach(tab => tab.addEventListener('click', onTabClick))
+btn.addEventListener('click', navToggle)
 
 function onTabClick(e) {
   tabs.forEach(tab => tab.children[0].classList.replace('border-softRed', 'border-white'))
@@ -49,4 +53,12 @@ function onTabClick(e) {
   const classString = e.target.getAttribute('data-target')
 
   document.getElementById('panels').getElementsByClassName(classString)[0].classList.replace('hidden', 'flex')
+}
+
+function navToggle(e) {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+  if (menu.classList.contains('flex')) logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  else logo.setAttribute('src', './images/logo-bookmark.svg')
 }
